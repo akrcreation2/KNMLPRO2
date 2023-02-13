8079c7cd8a032aedbfb3dde7f1f078793d9b9cae
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2023-02-13 20:58:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/KNMLPRO2 /Eva
+  git clone https://github.com/akrcreation2/KNMLPRO2 /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
